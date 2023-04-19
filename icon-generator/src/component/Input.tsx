8089ae9273926bcdbadb9b5c9d9c96aa9f189308
hojin hwang <hhwang1864@gmail.com>
2023-04-19 20:45:26 +1000

    adding first mutation request to trpc

---
+++ icon-generator/src/component/Input.tsx
@@ -0,0 +1,8 @@
+
+export function Input(props: React.ComponentPropsWithoutRef<"input">) {
+  return(
+    <input {...props} type="text" className="border border-gray-800 px4 py-2 dark:text-gray-800 rounded">
+
+    </input>
+  )
+}
