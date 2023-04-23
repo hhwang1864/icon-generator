c5427aeeeda4674ec8e5f960dcc9c02e6f9eed3e
hojin hwang <hhwang1864@gmail.com>
2023-04-24 01:12:53 +1000

    pre-deploy test2

--- icon-generator/src/component/Button.tsx
+++ icon-generator/src/component/Button.tsx
@@ -1,22 +1,27 @@
-import clsx from "clsx"
+import clsx from "clsx";
 import { Spinner } from "./Spinner";
 
-export function Button(props: React.ComponentPropsWithoutRef<"button"> & {
-  variant?: "primary" | "secondary";
-  isLoading?: boolean
+export function Button(
+  props: React.ComponentPropsWithoutRef<"button"> & {
+    variant?: "primary" | "secondary";
+    isLoading?: boolean;
   }
-  ) {
-    const color =
-      (props.variant ?? "primary") === 'primary'
-        ? 'bg-blue-400 hover:bg-blue-500'
-        : 'bg-gray-400 hover:bg-gray-500'
-  return(
+) {
+  const color =
+    (props.variant ?? "primary") === "primary"
+      ? "bg-blue-400 hover:bg-blue-500"
+      : "bg-gray-400 hover:bg-gray-500";
+
+  return (
     <button
       {...props}
-      className={clsx("flex gap-2 items-center justify-center rounded bg-blue-400 px-4 py-2 disabled:bg-gray-600", color)}
+      className={clsx(
+        "flex items-center justify-center gap-2 rounded px-4 py-2 disabled:bg-gray-600",
+        color
+      )}
     >
       {props.isLoading && <Spinner />}
       {props.children}
     </button>
-  )
+  );
 }
